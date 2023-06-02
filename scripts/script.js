272f569120b84f5e94cfe9a89c8d48cf7e737dad
Cristiano-Rodrigo <cristiano.rdasilva81@gmail.com>
2023-06-01 21:48:34 -0300

    Atualizacao menu hamburguer

--- scripts/script.js
+++ scripts/script.js
@@ -3,5 +3,9 @@ let menu = document.getElementById("menu_links")
 function mostrarMenu() {
     	if(window.getComputedStyle(menu).display == 'none') {
             // Mostre o menu
-        }
+            menu.style.display = "flex";
+             } else {
+            // Esconda o menu
+            menu.style.display = "none";
+             }
 }
