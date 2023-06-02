f8786fb3f71c59556b5a3efb71ca59f92a92c37d
Cristiano-Rodrigo <cristiano.rdasilva81@gmail.com>
2023-06-01 21:42:00 -0300

    atualizacao do javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
+let menu = document.getElementById("menu_links")
+
 function mostrarMenu() {
-    
+    	if(window.getComputedStyle(menu).display == 'none') {
+            // Mostre o menu
+        }
 }
